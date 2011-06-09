c0f60f251ee8f96530ead712fbec5bb99e006628
Seb Bacon <seb.bacon@gmail.com>
2011-06-09 15:09:18 +0100

    allow other members of my authzgroup to edit my packages

--- ckanext/datano/plugin.py
+++ ckanext/datano/plugin.py
@@ -1,15 +1,17 @@
 import os
 from logging import getLogger
 
-from genshi.filters.transform import Transformer
-
+from pylons import c
+from ckan import model
 from ckan.plugins import implements, SingletonPlugin
-from ckan.plugins import IConfigurer
+from ckan.plugins import IConfigurer, IPackageController
 
 log = getLogger(__name__)
 
+
 class DataNOPlugin(SingletonPlugin):
     implements(IConfigurer, inherit=True)
+    implements(IPackageController, inherit=True)
 
     def update_config(self, config):
         here = os.path.dirname(__file__)
@@ -27,4 +29,13 @@ class DataNOPlugin(SingletonPlugin):
         config['ckan.site_logo'] = "http://drupal1.computas.no/ckan/img/fad_logo_ckan.png"
         config['ckan.favicon'] = "http://drupal1.computas.no/ckan/img/favicon.ico"
         config['package_form'] = "datano_package_form"  # XXX change
-		
+
+    def create(self, package):
+        # take my first authzgroup
+        # and give it the editor role on this package
+        user = model.User.by_name(c.user)
+        authzgroups = user and user.authorization_groups
+        authzgroup = authzgroups and authzgroups[0]
+        if authzgroup:
+            role = "editor"
+            model.add_authorization_group_to_role(authzgroup, role, package)
